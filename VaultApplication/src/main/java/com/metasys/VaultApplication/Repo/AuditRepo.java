3eec65bf94c24458f778ea7aa07681a74a13c937
Rohith Goud Panjala <rohithgoud30@gmail.com>
2025-08-19 16:28:42 -0400

    Refactor(structure): rename Metasys Web Application directory to password_vault_web_application

--- VaultApplication/src/main/java/com/metasys/VaultApplication/Repo/AuditRepo.java
+++
@@ -1,9 +0,0 @@
-package com.metasys.VaultApplication.Repo;
-
-import com.metasys.VaultApplication.Model.AuditDTO;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuditRepo extends JpaRepository<AuditDTO, Long> { }
