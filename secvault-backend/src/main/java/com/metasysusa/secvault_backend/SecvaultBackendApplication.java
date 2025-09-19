7051ba58e99f61e071e979b01b3a45d53325202c
Rohith Goud Panjala <rohithgoud30@gmail.com>
2025-09-18 21:59:53 -0400

    Add backend folder and initial files

---
+++ secvault-backend/src/main/java/com/metasysusa/secvault_backend/SecvaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.metasysusa.secvault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecvaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecvaultBackendApplication.class, args);
+	}
+
+}
